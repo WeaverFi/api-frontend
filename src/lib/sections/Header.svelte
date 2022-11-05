ce70c91d84efa00ba58d3509115ef1420c5ebd72
Ncookiez <ncukiert@gmail.com>
2022-11-05 13:29:38 -0300

    Minor fixes

--- src/lib/sections/Header.svelte
+++ src/lib/sections/Header.svelte
@@ -8,7 +8,7 @@
 <!-- #################################################################################################### -->
 
 <section>
-	<h3>The WeaverFi API is the easiest way to Make the most out of WeaverFi functionality!</h3>
+	<h3>The WeaverFi API is the easiest way to make the most out of WeaverFi functionality!</h3>
 	<p>Unlike other APIs, you can get your API keys and manage them entirely on&#8209;chain. This is made possible with <a href="https://github.com/3PIKeys" target="_blank" rel="noreferrer">3PI</a>, an open&#8209;source, on&#8209;chain key management system.</p>
 	<p>Get a key for free to try out the API, or pay for a higher rate limit using <strong>USDC</strong> in a fully trustless manner. If the API doesn't fit your needs, withdraw your unused funds anytime.</p>
 	<p>Both the API's code and this front&#8209;end's code are open&#8209;source and available on our GitHub!</p>
